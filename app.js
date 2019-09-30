f91b861cf0124bc574504011971aaae985979672
Cadena <206613806@tfayd.com>
2019-09-30 13:39:02 -0500

    new rows added and styling changes were adding the site

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ $(document).ready(function () {
 		changetype = $('#change-type').val().trim();
 		status = $('#status').val().trim();
 		priority = $('#priority').val().trim();
-		impact = $('impact').val().trim();
+		impact = $('#impact').val().trim();
+		risk = $('#risk').val().trim();
+		group = $('#group').val().trim();
+		agent = $('#agent').val().trim();
+		description = $('#description').val().trim();
 		startdate = $('#employee-startdate').val().trim();
 		enddate = $('#employee-enddate').val().trim();
 
@@ -34,6 +38,9 @@ $(document).ready(function () {
 			status: status,
 			priority: priority,
 			impact: impact,
+			group: group,
+			agent: agent,
+			description: description,
 			startdate: startdate,
 			enddate: enddate,
 			dateAdded: firebase.database.ServerValue.TIMESTAMP
@@ -45,6 +52,9 @@ $(document).ready(function () {
 		$('#status').val('');
 		$('#priority').val('');
 		$('#impact').val('');
+		$('#group').val('');
+		$('#agent').val('');
+		$('#description').val('');
 		$('#employee-startdate').val('');
 		$('#employee-enddate').val('');
 
@@ -62,6 +72,13 @@ $(document).ready(function () {
 		newRow = $('<div>').addClass('employee row');
 		nameCol = $('<div class="col-xs-2">' + snapVal.name + '</div>');
 		changerequestedCol = $('<div class="col-xs-2">' + snapVal.changerequested + '</div>');
+		changetypeCol = $('<div class="col-xs-2">' + snapVal.changetype + '</div>');
+		statusCol = $('<div class="col-xs-2">' + snapVal.status + '</div>');
+		priorityCol = $('<div class="col-xs-2">' + snapVal.priority + '</div>');
+		impactCol = $('<div class="col-xs-2">' + snapVal.impact + '</div>');
+		groupCol = $('<div class="col-xs-2">' + snapVal.group + '</div>');
+		agentCol = $('<div class="col-xs-2">' + snapVal.agent + '</div>');
+		descriptionCol = $('<div class="col-xs-2">' + snapVal.description + '</div>');
 		dateCol = $('<div class="col-xs-2">' + snapVal.startdate + '</div>');
 		enddateCol = $('<div class="col-xs-2">' + snapVal.enddate + '</div>');
 
@@ -72,6 +89,9 @@ $(document).ready(function () {
 		newRow.append(status);
 		newRow.append(priority);
 		newRow.append(impact);
+		newRow.append(group);
+		newRow.append(agent);
+		newRow.append(description);
 		newRow.append(dateCol);
 		newRow.append(enddateCol);
 
